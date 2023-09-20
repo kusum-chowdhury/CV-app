2b4a2141cc942da8e952819c2b3f09234e536bb5
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-20 11:02:31 -0700

    add workexperienc component to input work experience of the user

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../styles/InputField.css'
 import InputGenInfo from './InputGenInfo'
 import InputEducation from './InputEducation'
+import InputWorkExperience from './InputWorkExperience'
 
 function InputField() {
   return (
     <div className='inputs'>
         <InputGenInfo />
         <InputEducation />
+        <InputWorkExperience />
     </div>
   )
 }
