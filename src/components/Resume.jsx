e91516ac584e0f84383112c0c91c62029c0f5d04
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-22 12:41:58 -0700

    add styling to the workexperience section of the resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -11,15 +11,15 @@ function Resume(props) {
       <hr />
       <h4>Education</h4>
       <div className='education'>
-      <p className='degree'>{props.degree}</p>
-      <p>{props.school}</p>
+      <p className='degree upper-case'>{props.degree}</p>
+      <p className='cap'>{props.school}</p>
       <p className='date'>{props.start}</p>
       <p className='date'> - {props.end}</p>
       </div>
       <h4>Work Experience</h4>
       <div className='work-exp'>
-      <p>{props.job}</p>
-      <p>{props.company}</p>
+      <p className='upper-case'>{props.job}</p>
+      <p className='cap'>{props.company}</p>
       <p className='date'>{props.start1}</p>
       <p className='date'> - {props.end1}</p>
       <p>{props.description}</p>
